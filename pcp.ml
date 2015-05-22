32c59ebac3244f34fe596adf5197a2efeaa42e7a
Keisuke Nakano <ksk1227@gmail.com>
2015-05-22 12:14:51 +0900

    Changes the name of type variables and the order of sequences.

--- pcp.ml
+++ pcp.ml
@@ -4,15 +4,15 @@
 type a and b and ok and empty
 
 (* PCP pairs *)
-type (_, 't1,'t2) pair =
+type (_, _, _) pair =
   | Empty : (empty, unit, unit) pair
-  | Pair1 : (_, 't1,'t2) pair -> (ok, a*'t1, b*(a*(a*'t2))) pair
-  | Pair2 : (_, 't1,'t2) pair -> (ok, a*(b*'t1), a*(a*'t2)) pair
-  | Pair3 : (_, 't1,'t2) pair -> (ok, b*(b*(a*'t1)), b*(b*'t2)) pair
+  | Pair1 : (_, 'seq1,'seq2) pair -> (ok,   'seq1*a      , (('seq2*b)*a)*a) pair
+  | Pair2 : (_, 'seq1,'seq2) pair -> (ok,  ('seq1*a)*b   ,  ('seq2*a)*a   ) pair
+  | Pair3 : (_, 'seq1,'seq2) pair -> (ok, (('seq1*b)*b)*a,  ('seq2*b)*b   ) pair
 
 (* A complete GADT-exhaustivity checker should warn 
    if and only if the PCP has a solution. *)
 let f : (ok, 't,'t) pair option -> unit = function None -> ()
 
 (* The PCP above is the case where a solution exists. *)
-let _ : (ok, 't,'t) pair = Pair3 (Pair2 (Pair3 (Pair1 Empty)))
+let x : (ok, 't,'t) pair = Pair1 (Pair3 (Pair2 (Pair3 Empty)))
